c11318762a76c4f242e2270b0d293bbf9f776fe9
grimbn <bhasin.agrim996@gmail.com>
2021-05-11 21:16:51 +0530

    Unfinished Ball Physics + Ball Spawning

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
 public class BallControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D ballRigidbody2D;
+    CircleCollider2D ballCollider2D;
+    Touch touch;
+    [SerializeField] float minFingerSpeed = 0.1f, minYMoveDistance = 0.2f, verticalForce = 5f;
+
     void Start()
     {
-        
+        Input.simulateMouseWithTouches = false;
+        ballRigidbody2D = GetComponent<Rigidbody2D>();
+        ballCollider2D = GetComponent<CircleCollider2D>();        
     }
 
-    // Update is called once per frame
+    private void OnMouseDown()
+    {        
+        LaunchBall();
+    }
+
     void Update()
     {
-        
+        ProcessTouchInput();
+    }
+
+    private void ProcessTouchInput()
+    {
+        Vector2 initialTouchPos, newTouchPos;
+
+        if(Input.touchCount > 0 && ballCollider2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position)))
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                initialTouchPos = touch.position;
+                newTouchPos = initialTouchPos;
+                //ChangeBallPos(newTouchPos);
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                newTouchPos = touch.position;
+                //ChangeBallPos(newTouchPos);
+            }  
+            
+            if(touch.phase == TouchPhase.Ended)
+            {
+                newTouchPos = touch.position;
+                float fingerSpeed = touch.deltaPosition.magnitude / touch.deltaTime;
+                if(fingerSpeed > minFingerSpeed && touch.deltaPosition.y > minYMoveDistance)
+                {
+                    LaunchBall();
+                }
+            }
+        }
+    }
+
+    private void LaunchBall()
+    {
+        ballRigidbody2D.constraints = RigidbodyConstraints2D.None;
+        ballRigidbody2D.AddForce(new Vector2(0f, verticalForce),ForceMode2D.Impulse);
+    }
+
+    private void ChangeBallPos(Vector2 screenPos)
+    {
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        ballRigidbody2D.position = new Vector2(worldPos.x,ballRigidbody2D.position.y);
     }
 }
---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BallSpawner : MonoBehaviour
+{
+    BoxCollider2D ballDespawnCollider;
+    [SerializeField] GameObject ballPrefab;
+
+    void Start()
+    {
+        ballDespawnCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {        
+        if(other.gameObject.GetComponent<BallControl>())
+        {
+            Destroy(other.gameObject);
+            Instantiate(ballPrefab, new Vector3( Random.Range(-2f, 2f), -4f, 0f),Quaternion.identity);
+        }
+    }
+
+}
